48898d2c15824e950ea7d1e97bf1f29184acac7e
AnderLopezGarcia <dam32.ander.lopez@gmail.com>
2023-05-12 16:49:55 +0200

    asdfasd

--- src/app/shared/components/game-card/game-card.component.ts
+++ src/app/shared/components/game-card/game-card.component.ts
@@ -5,9 +5,14 @@ import { Result } from 'src/app/interfaces/Videojuego.interfaces';
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: [`./game-card.component.css`]
+  
 })
 export class GameCardComponent implements OnInit {
   @Input() juego!: Result;
+  listas: { icon: string, nombre: string }[] = [
+    { icon : "favorite", nombre : "Seguidos"},
+    { icon : "watch_later", nombre : "Más tarde"},
+    { icon : "clear", nombre : "Abandonados"},]
   constructor() { }
 
   ngOnInit() {
