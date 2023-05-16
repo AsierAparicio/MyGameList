6684ad3cb1f39287a9cb7a2d11c163ae47f625db
Iker <ikermurga74@gmail.com>
2023-05-16 13:29:40 +0200

    main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
     component: GameListPersonalComponent,
   },
   {
+    path: 'directorio',
+    component: GameListComponent,
+  },
+  {
     path: '**',
     redirectTo: ''
   }
